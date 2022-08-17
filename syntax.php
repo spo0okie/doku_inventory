2ff13bf179318dc2fee88974af885c77159917f5
reviakin@yoga14 <reviakin@gmail.com>
2022-08-17 19:46:37 +0500

    добавлены ip

--- syntax.php
+++ syntax.php
@@ -172,7 +172,11 @@ class syntax_plugin_inventory extends DokuWiki_Syntax_Plugin
 				}
 				break;
 
-            case 'org-phones':
+			case 'ip':
+				return $this->fetchInventoryPage($api.'/ip/item-by-name?name='.urlencode($id),$name_replacement);
+				break;
+
+			case 'org-phones':
                 if (is_numeric($id)) {
                     return $this->fetchInventoryPage($api.'/'.$controller.'/item?id='.$id,$name_replacement);
                 } return 'Поддерживается ссылка только через ID';
